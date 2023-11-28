84fb358f6b01a81c2c45201205b3cee497d5f5f7
AminaKislota2003 <151931602+AminaKislota2003@users.noreply.github.com>
2023-11-28 15:48:15 +0300

    Add files via upload

---
+++ rhd8sTw4.py
@@ -0,0 +1,16 @@
+import telebot
+banan=telebot.TeleBot("6859145466:AAEfzbtPJdcqfvv5p_MUGFmACi1DUQt0jQc")
+
+@banan.message_handler(commands={"start"})
+def main(message):
+    bot.send_message(message.chat.id, 'Артем краш')
+
+@banan.message_handler(commands={"Да ?"})
+def main(message):
+    bot.send_message(message.chat.id, '*А вы не верили* \n а я уверен в себе',parse_mode='Markdown')
+
+@banan.message_handler(commands={"Нет"})
+def main(message):
+    bot.send_message(message.chat.id, 'я хотел сделать выбор Да либо Нет ')
+
+bot.infinity_polling()
